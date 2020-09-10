cbbadcd06bc2a8df95e06d8ba831750e674fbff0
Art <freddieshughes@googlemail.com>
2020-09-10 12:45:13 +0100

    serving a template html

--- server.go
+++ server.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Homepage",
 		})
 	})
-	r.Run()
+	r.Run(":8080")
 }
