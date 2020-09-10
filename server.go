bb7871adc0b448dae6336b060aeacbdd32909b1c
Art <freddieshughes@googlemail.com>
2020-09-10 12:55:59 +0100

    adding a test POST route

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"fmt"
 
 	"net/http"
 )
@@ -9,10 +10,15 @@ import (
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Homepage",
 		})
+	})
+
+	r.POST("/", func(c *gin.Context) {
+		fmt.Println("POST RECEIVED")
 	})
 	r.Run(":8080")
 }
