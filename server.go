9759dc2aa4c23d89d78326f5d609ee07d18bd39a
Art <freddieshughes@googlemail.com>
2020-09-14 10:26:15 +0100

    adding quoteGenerator and making get / return a random quote as a json object

--- server.go
+++ server.go
@@ -2,19 +2,48 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"fmt"
 
+	"fmt"
+	"os"
+	"math/rand"
+	"io/ioutil"
+	"encoding/json"
 	"net/http"
 )
+
+func quoteGenerator() (string) {
+	jsonFile, err := os.Open("quotes.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("successfully opened JSON file.")
+
+	defer jsonFile.Close()
+
+	byt, _ := ioutil.ReadAll(jsonFile)
+
+	var res map[string]interface{}
+	json.Unmarshal([]byte(byt), &res)
+
+	quotes := res["quotes"].([]interface{})
+	quote := quotes[rand.Intn(len(quotes))].(string)
+
+	return quote
+}
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Homepage",
-		})
+		quote := quoteGenerator()
+		var response struct {
+			Quote string
+		}
+		response.Quote = quote
+		c.JSON(http.StatusOK, response)
 	})
 
 	r.POST("/", func(c *gin.Context) {
